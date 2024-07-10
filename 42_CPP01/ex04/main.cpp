fc9ccf8ec2cdd7367a2502e91a9d3bd8fd5efac5
arthurmalangin <amalangin@icloud.com>
2024-07-10 21:05:57 +0200

    add ex04

---
+++ 42_CPP01/ex04/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+
+
+void stringReplace(std::string &line, std::string toSearch, std::string toReplace)
+{
+    size_t pos = line.find(toSearch);
+    while (pos != std::string::npos) {
+        line.erase(pos, toSearch.size());
+        line.insert(pos, toReplace);
+        pos = line.find(toSearch);
+    }
+}
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        std::cout << "Error: Invalid number of argument\'s" << std::endl;
+        return (1);
+    }
+    std::ifstream file(argv[1]);
+    if (file.is_open()) {
+        std::string content;
+        std::string line;
+        std::string s1 = argv[2];
+        std::string s2 = argv[3];
+
+        while (std::getline(file, line)) {
+            content += line + "\n";
+        }
+        stringReplace(content, s1, s2);
+        std::ofstream toExport(std::string(argv[1]) + ".replace");
+        if (toExport.is_open()) {
+            toExport << content;
+            toExport.close();
+        } else {
+            std::cout << "Error: can't create " << argv[1] << " !" << std::endl;    
+        }
+        file.close();
+    } else {
+        std::cout << "Error: can't open file !" << std::endl;
+        return (1);
+    }
+    return (0);
+}
