fe776e772e28bc06e1ff6f1b3c8975d1fb059893
Arthur <amalangin@icloud.com>
2024-10-27 01:58:31 +0200

    Final Push

--- 42_CPP04/ex03/main.cpp
+++ 42_CPP04/ex03/main.cpp
@@ -3,48 +3,20 @@
 #include "MateriaSource.hpp"
 #include "Character.hpp"
 
-int main(void) {
+int main()
+{
 	IMateriaSource* src = new MateriaSource();
-
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-
-	//test if full
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-
 	ICharacter* me = new Character("me");
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-
-	//test if full
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-
 	ICharacter* bob = new Character("bob");
-	*(Character *)bob = *(Character *)me;
 	me->use(0, *bob);
-	bob->use(0, *bob);
 	me->use(1, *bob);
-	me->use(2, *bob);
-	me->use(3, *bob);
-	me->use(4, *bob);
-	me->use(5, *bob);
-	tmp = me->getInv(0);
-	me->unequip(0);
-	delete tmp;
-	tmp = bob->getInv(1);
-	bob->unequip(1);
-	delete tmp;
 	delete bob;
 	delete me;
 	delete src;
